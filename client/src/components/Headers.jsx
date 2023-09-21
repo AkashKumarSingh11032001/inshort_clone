b8b023d1eea5b7b4f9b848f3c9d99c8575403eda
Akash Singh <akashkumarsingh11032001@gmail.com>
2023-09-21 16:44:51 +0530

    header section completed

--- client/src/components/Headers.jsx
+++ client/src/components/Headers.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import { AppBar, Toolbar, styled } from "@mui/material";
+import { Menu } from "@mui/icons-material";
+
+const StyleHeader = styled(AppBar)`
+  background-color: #fff;
+  height: 70px;
+`
+const MenuIcon = styled(Menu)`
+color: #000;
+`
+
+const Image = styled("img")({
+  height: 55,
+  margin: "auto",
+  paddingRight: 70,
+})
 
 const Headers = () => {
-  return (
-    <div>Headers</div>
-  )
-}
+  const url = 'https://assets.inshorts.com/website_assets/images/logo_inshorts.png';
 
-export default Headers
+  return (
+    <StyleHeader>
+      <Toolbar>
+        <MenuIcon />
+        <Image src={url} alt="logo" />
+      </Toolbar>
+    </StyleHeader>
+  );
+};
+
+export default Headers;
