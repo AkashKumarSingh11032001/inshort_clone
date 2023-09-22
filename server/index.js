4f902ae785ce3d3a2ea6d56ba9d55c77fa16ba9e
Akash Singh <akashkumarsingh11032001@gmail.com>
2023-09-22 12:31:29 +0530

    news data

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import Connection from './database/db.js';
 
+// Express app creation
 const app = express();
-
-
 const PORT = 8000;
+
+// DB connection
 Connection();
 
+
+// Port listen
 app.listen(PORT, () => {
     console.log(`Server running at PORT ${PORT}`)
 })
