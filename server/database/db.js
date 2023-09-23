6ae61d5e2703a014b408d1f0c08a900833bbb398
Akash Singh <akashkumarsingh11032001@gmail.com>
2023-09-23 09:40:55 +0530

    init check done

--- server/database/db.js
+++ server/database/db.js
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
 
 const Connection = async () => {
-  const URL = `mongodb+srv://akashkumarsingh11032001:iLkXALnppwcOXAOR@cluster0.lutgzyg.mongodb.net/?retryWrites=true&w=majority`;
+  const URL = process.env.MONGODB_URI;
   try {
-    await mongoose.connect(URL, { useNewUrlParser: true });
+    await mongoose.connect(URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
     console.log("Database connected Successfully!");
   } catch (err) {
     console.log(`Error while connecting with the database`, err);
--- server/index.js
+++ server/index.js
@@ -3,12 +3,15 @@ import Connection from './database/db.js';
 import DefaultData from './default.js';
 import Route from './routes/routes.js';
 import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
 
 // Express app creation
 const app = express();
 const PORT = 8000;
 
 // Website route
+app.use(cors());
 app.use('/', Route);
 
 // DB connection
