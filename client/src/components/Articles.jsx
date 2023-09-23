63a21081a7ba06bf14c81fc2c5c68654e0531a6f
Akash Singh <akashkumarsingh11032001@gmail.com>
2023-09-23 10:09:08 +0530

    article card

--- client/src/components/Articles.jsx
+++ client/src/components/Articles.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Box } from "@mui/material";
 import { useEffect, useState } from "react";
 import { getNews } from "../services/api";
+import ArticlesCard from "./ArticlesCard";
 
 const Articles = () => {
   const [news, setNews] = useState([]);
@@ -12,14 +13,14 @@ const Articles = () => {
 
   const dailyNews = async () => {
     let response = await getNews();
-    console.log(response.data);
+    // console.log(response.data);
     setNews(response.data);
   };
   return (
     <Box>
-      {news.map((data) => {
-        console.log(data);
-      })}
+      {news.map((data) => (
+        <ArticlesCard />
+      ))}
     </Box>
   );
 };
---
+++ client/src/components/ArticlesCard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ArticlesCard = () => {
+  return (
+    <p>Data</p>
+  )
+}
+
+export default ArticlesCard
